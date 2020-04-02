d3d70249ad3ef703708be38411686cedcb175e3a
mcik <micaellabelle@gmail.com>
2020-04-02 16:19:54 -0400

    begin dev

---
+++ src/main/java/com/mickdev/Referrences.java
@@ -0,0 +1,9 @@
+package com.mickdev;
+
+public class Referrences {
+    public static final String MODID="ultimatepvpfactionmods";
+    public static final String NAME="Ultimate PVP Faction Mods";
+    public static final String VERSION="1.12.2";
+
+    //Proxy
+}
---
+++ src/main/java/com/mickdev/ultimatepvpfactionmods.java
@@ -0,0 +1,10 @@
+package com.mickdev;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Referrences.MODID,name = Referrences.NAME,version = Referrences.VERSION)
+public class ultimatepvpfactionmods {
+
+
+
+}
